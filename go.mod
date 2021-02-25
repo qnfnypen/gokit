1caeb5bce4e805b29101f5a3614e986d0adcf2e5
hanchan <baozhihao@tingyutech.com>
2021-02-25 10:35:26 +0800

    update generatelogger

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/qnfnypen/gokit
 
 go 1.16
 
-require github.com/rs/zerolog v1.20.0
+require (
+	github.com/mitchellh/go-homedir v1.1.0
+	github.com/rs/zerolog v1.20.0
+)
--- logger/log.go
+++ logger/log.go
@@ -1,32 +1,60 @@
 package logger
 
 import (
+	"errors"
 	"fmt"
 	"os"
 	"path/filepath"
 	"strings"
 	"time"
 
+	"github.com/mitchellh/go-homedir"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 )
 
 // GenerateLogger 生成日志引擎
-// dirPath：日志保存路径（文件夹），如果输入的是文件，则截取文件夹路径
+// dirPath：日志保存路径（文件夹），如果输入的是文件，则截取文件夹路径，支持. ~，格式为./log
 // cycle：日志保存周期，即按天(day)，按月(month)，按年(year)等方式进行保存
 func GenerateLogger(dirPath string, cycle string) (zerolog.Logger, error) {
-	fileDir := filepath.Dir(dirPath)
 	var filePath string
+
+	dirPath = strings.TrimSpace(dirPath)
+
+	if filepath.Ext(dirPath) != "" {
+		return zerolog.Logger{}, errors.New("dirPath格式错误")
+	}
+
+	lastch := string([]rune(dirPath)[len([]rune(dirPath))-1:])
+	if lastch != "/" {
+		dirPath += "/"
+	}
+
+	ch := string([]rune(dirPath)[1:])
+	firstch := string([]rune(dirPath)[:1])
+	if firstch != "." && firstch != "~" && firstch != "/" {
+		return zerolog.Logger{}, errors.New("dirPath格式错误")
+	}
+
+	switch firstch {
+	case ".":
+		pwd, _ := os.Getwd()
+		dirPath = pwd + ch
+	case "~":
+		h, _ := homedir.Dir()
+		dirPath = h + ch
+	}
+
 
 	switch strings.ToLower(cycle) {
 	case "day":
-		filePath = fmt.Sprintf("%s/%s.log", fileDir, time.Now().Format("20060102"))
+		filePath = fmt.Sprintf("%s/%s.log", dirPath, time.Now().Format("20060102"))
 	case "month":
-		filePath = fmt.Sprintf("%s/%s.log", fileDir, time.Now().Format("200601"))
+		filePath = fmt.Sprintf("%s/%s.log", dirPath, time.Now().Format("200601"))
 	case "year":
-		filePath = fmt.Sprintf("%s/%s.log", fileDir, time.Now().Format("200601"))
+		filePath = fmt.Sprintf("%s/%s.log", dirPath, time.Now().Format("200601"))
 	default:
-		filePath = fmt.Sprintf("%s/log.log", fileDir)
+		filePath = fmt.Sprintf("%s/log.log", dirPath)
 	}
 
 	f, err := os.OpenFile(filePath, os.O_CREATE|os.O_APPEND|os.O_RDONLY, 0666)
